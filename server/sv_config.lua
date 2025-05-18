8e2d5e02cf60c7b6f3f8c1055271e368edb8c336
MaDHouSe <madhouse1979@gmail.com>
2025-05-18 11:33:10 +0200

    Update sv_config.lua

--- server/sv_config.lua
+++ server/sv_config.lua
@@ -7,6 +7,7 @@ SV_Config.Peds = Peds
 SV_Config.Vehicles = Vehicles
 
 SV_Config.ResetTimer = 30 -- 30 * 1000
+SV_Config.DisableRunningWhenCuffed = false
 
 SV_Config.JailCoords = vector3(441.0086, -981.1292, 30.6896)
 -- Make sure you have the items in your system.
